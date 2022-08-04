aa1800d751bedd85ae4f21462f119ad395c72f1d
cmd430 <cmd430@gmail.com>
2022-08-04 17:29:38 +1200

    remove unnecessary function call

--- Better Fullscreen/BetterFullscreen.vb
+++ Better Fullscreen/BetterFullscreen.vb
@@ -42,7 +42,6 @@ Public Class BetterFullscreen
             TrayIcon.Icon = My.Resources.Fullscreen_Light
         End If
 
-        SetTitleBarTheme(Handle)
         ToggleWindowState(Me, IIf(Debugger.IsAttached Or Not Config.Settings.StartHidden, FormWindowState.Normal, FormWindowState.Minimized))
         Init()
     End Sub
